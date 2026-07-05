d4b05f7f2e9bb246f8ff4d960438c0396ce69837
Omar Mohamed <omarmoh3010@gmail.com>
2026-07-05 17:55:54 +0300

    edit astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://ashrysteel.com',
+  site: 'https://astronaut.gijthub.io',
+  base:'/https://github.com/0mar3010/ashrysteel',
   compressHTML: true,
 });
